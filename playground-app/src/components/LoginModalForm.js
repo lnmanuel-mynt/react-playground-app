211274bcfb680cb9e79cf4c417065f2fd1623cce
Lester Manuel <lester.manuel@mynt.xyz>
2021-05-28 13:39:42 +0800

    used form group component

--- playground-app/src/components/LoginModalForm.js
+++ playground-app/src/components/LoginModalForm.js
@@ -1,38 +1,33 @@
-import './Form.css'
+import FormGroup from './FormGroup'
+import '../styles/Form.css'
 
 const LoginModalForm = props => {
     return (
-        <>
-            <form className="form" onSubmit={props.onFormSubmit}>
-                <div className="form-body">
-                    <header className="form-control">Login</header>
-                    <div className="form-control">
-                        <div className="form-group">
-                            <div>
-                                <label htmlFor="email">Email</label>
-                            </div>
-                            <div>
-                                <input required type="text" value={props.emailValue} onChange={props.onEmailChange}></input>
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <div>
-                                <label htmlFor="password">Password</label>
-                            </div>
-                            <div>
-                                <input required type="password" value={props.passwordValue} onChange={props.onPasswordChange}></input>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="form-control">
-                        <button className="btn-submit" type="submit">Login</button>
-                    </div>
-                    <div className="form-footer" onClick={props.onSignupClick}>
-                        Don't have an account? Sign up
-                        </div>
+        <form className="form" onSubmit={props.onFormSubmit}>
+            <div className="form-body">
+                <header className="form-control">Login</header>
+                <div className="form-control">
+                    <FormGroup
+                        htmlFor="email"
+                        labelText="Email"
+                        inputType="text"
+                        inputValue={props.emailValue}
+                        onInputChange={props.onEmailChange} />
+                    <FormGroup
+                        htmlFor="password"
+                        labelText="Password"
+                        inputType="password"
+                        inputValue={props.passwordValue}
+                        onInputChange={props.onPasswordChange} />
                 </div>
-            </form>
-        </>
+                <div className="form-control">
+                    <button className="btn-submit" type="submit">Login</button>
+                </div>
+                <div className="form-footer" onClick={props.onSignupClick}>
+                    Don't have an account? Sign up
+                    </div>
+            </div>
+        </form>
     )
 }
 
--- playground-app/src/components/SignupModalForm.js
+++ playground-app/src/components/SignupModalForm.js
@@ -1,46 +1,39 @@
-import './Form.css'
+import FormGroup from './FormGroup'
+import '../styles/Form.css'
 
 const SignupModalForm = props => {
     return (
-        <>
-            <form className="form" onSubmit={props.onFormSubmit}>
-                <div className="form-body" >
-                    <header className="form-control">Sign Up</header>
-                    <div className="form-control">
-                        <div className="form-group">
-                            <div>
-                                <label htmlFor="displayName">Display Name</label>
-                            </div>
-                            <div>
-                                <input required type="text" value={props.displayNameValue} onChange={props.onDisplayNameChange}></input>
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <div>
-                                <label htmlFor="email">Email</label>
-                            </div>
-                            <div>
-                                <input required type="text" value={props.emailValue} onChange={props.onEmailChange}></input>
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <div>
-                                <label htmlFor="password">Password</label>
-                            </div>
-                            <div>
-                                <input required type="password" value={props.passwordValue} onChange={props.onPasswordChange}></input>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="form-control">
-                        <button className="btn-submit" type="submit">Sign Up</button>
-                    </div>
-                    <div className="form-footer" onClick={props.onLoginClick}>
-                        Have an account? Login
-                        </div>
+        <form className="form" onSubmit={props.onFormSubmit}>
+            <div className="form-body" >
+                <header className="form-control">Sign Up</header>
+                <div className="form-control">
+                    <FormGroup
+                        htmlFor="displayName"
+                        labelText="Display Name"
+                        inputType="text"
+                        inputValue={props.displayNameValue}
+                        onInputChange={props.onDisplayNameChange} />
+                    <FormGroup
+                        htmlFor="email"
+                        labelText="Email"
+                        inputType="text"
+                        inputValue={props.emailValue}
+                        onInputChange={props.onEmailChange} />
+                    <FormGroup
+                        htmlFor="password"
+                        labelText="Password"
+                        inputType="password"
+                        inputValue={props.passwordValue}
+                        onInputChange={props.onPasswordChange} />
                 </div>
-            </form>
-        </>
+                <div className="form-control">
+                    <button className="btn-submit" type="submit">Sign Up</button>
+                </div>
+                <div className="form-footer" onClick={props.onLoginClick}>
+                    Have an account? Login
+                </div>
+            </div>
+        </form>
     )
 }
 
